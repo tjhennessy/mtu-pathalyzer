a52091324371683ca25e216dadbcb6658c0c79d3
Timothy Hennessy <tj@spyegomedia.com>
2021-10-27 17:39:17 -0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e 
+
+# check python version is 3
+
+# make sure pip is installed
+
+# create venv
+
+# pip install -r requirements.txt
+
+# activate venv
+
+# call program with --help
